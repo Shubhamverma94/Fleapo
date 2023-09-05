7e9368de9e7b32ea15d1b08f6b395578280cd3f2
Shubham Verma <imkannu.shubham@gmail.com>
2023-09-05 18:32:31 +0530

    add

--- fleapo/src/test/java/Fleapo.java
+++ fleapo/src/test/java/Fleapo.java
@@ -1,4 +1,6 @@
 import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
@@ -6,7 +8,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Fleapo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriverManager.edgedriver().setup();
         EdgeOptions options = new EdgeOptions();
         WebDriver driver = new EdgeDriver(options);
@@ -14,9 +16,37 @@ public class Fleapo {
         driver.get("https://wishbox2.vercel.app/");
         driver.manage().window().maximize();
         
-        // Add your automation code here
+        driver.findElement(By.className(".form-control ")).sendKeys("Shubham Verma");
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
         
-        // Don't forget to close the driver when done
+        Thread.sleep(5);
+        
+        driver.findElement(By.xpath("//h2[text()='Enter OTP']")).isDisplayed();
+        
+        driver.findElement(By.xpath("//input[@aria-label='Please enter verification code. Digit 1']")).sendKeys("123456");
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
+        
+        Thread.sleep(5);
+        
+        driver.findElement(By.xpath("//div[@class='add-new-p-card flex-center-center text-center']")).click();
+        
+        driver.findElement(By.xpath("//h2[text()='Create new wishlist']")).isDisplayed();
+        
+        driver.findElement(By.xpath("//input[@placeholder='Wishlist Name']")).sendKeys("Shubham Verma");
+        
+        driver.findElement(By.id("description")).sendKeys("Demo");
+        
+        driver.findElement(By.id("upload-cover-input")).sendKeys("https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.shorturl.at%2Fimg%2Fshorturl-icon.png&tbnid=BItbp5rBnoFabM&vet=12ahUKEwiM_aykwpOBAxVKSmwGHc61BfwQMygAegQIARBz..i&imgrefurl=https%3A%2F%2Fwww.shorturl.at%2F&docid=8m5cWULGS8amNM&w=350&h=350&q=image%20short%20url&ved=2ahUKEwiM_aykwpOBAxVKSmwGHc61BfwQMygAegQIARBz");
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
+        
+        Thread.sleep(5);
+        
+        driver.findElement(By.xpath("//h2[text()='Shubham Verma']")).isDisplayed();
+        
+        Thread.sleep(5);
+        
         driver.quit();
+        
+       
     }
 }
